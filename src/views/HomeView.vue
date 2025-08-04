8deac307cf6a1e9fa8726e93090ab029d176d42a
Abdul Mannan <chamaankhan011@gmail.com>
2025-08-04 19:42:21 +0530

    Click handler

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,36 @@
 <template>
-  <h1>{{ message }}</h1>
+  <button v-on:click="decrement()">--</button>
+  {{ counter }}
+  <button @click="increment()">++</button>
 </template>
 
+<!-- <script>
+// option api
+export default {
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.counter++
+    },
+    decrement() {
+      this.counter--
+    },
+  },
+}
+</script> -->
+
 <script setup>
-const message = 'Hello, Vue'
+import { ref } from 'vue'
+let counter = ref(0)
+
+function increment() {
+  counter.value++
+}
+function decrement() {
+  counter.value--
+}
 </script>
